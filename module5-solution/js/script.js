35990fa77dbc31414c6d8aab7c0388e0a1fcb17c
unknown <radoslav.bonev@netea.bggit congif --global user.name Radoslav>
2017-09-03 17:55:35 +0300

    removed the console log

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -104,7 +104,6 @@ function buildAndShowHomeHTML (categories) {
       // variable's name implies it expects.
        var chosenCategoryShortName = chooseRandomCategory(categories);
 
-       console.log(chosenCategoryShortName);
 
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
